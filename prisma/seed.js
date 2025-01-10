b84f8235a1b259285761ef7b842a09526fdead49
MatiasCanevaro <mcanevaro@frba.utn.edu.ar>
2025-01-10 01:17:40 -0300

    fix seed.js x2

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,15 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function createTableURL() {
+async function checkIfTableExists() {
   try {
-    const result = await prisma.$queryRaw`SELECT to_regclass('public.url')`;
-    if (!result[0].to_regclass) {
+    // Execute a query to check if the `url` table exists in the database and cast the result to a string
+    const result = await prisma.$queryRaw`SELECT to_regclass('public.url')::text`;
+
+    if (!result || !result[0] || !result[0].to_regclass) {
       console.log("The `url` table does not exist. Creating the table...");
 
+      // Create the table manually if it does not exist
       await prisma.$executeRaw`
         CREATE TABLE public.url (
           id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -26,7 +29,7 @@ async function createTableURL() {
 }
 
 async function main() {
-  await createTableURL();
+  await checkIfTableExists();
   await prisma.$disconnect();
 }
 
